0d9395cb1d26d0d659cf211806634d7ddefba12d
Steve <me@stevelongoria.net>
2016-08-15 10:18:51 -0700

    Configured create_subscription.php & updated index.html content a little

--- create_subscription.php
+++ create_subscription.php
@@ -5,22 +5,22 @@ require_once('vendor/autoload.php');
 
 // Be sure to replace this with your actual test API key
 // (switch to the live key later)
-\Stripe\Stripe::setApiKey("sk_test_gSNxLnM5VJDjc0UMTDjU0akT");
+\Stripe\Stripe::setApiKey("pk_test_taCh3JS787HviV2oTE0NWUxW");
 
 try
 {
   $customer = \Stripe\Customer::create(array(
     'email' => $_POST['stripeEmail'],
     'source'  => $_POST['stripeToken'],
-    'plan' => 'weekly_box'
+    'plan' => 'basic_bean'
   ));
 
-  header('Location: thankyou.html');
+  header('Location: ty2821.html');
   exit;
 }
 catch(Exception $e)
 {
-  header('Location:oops.html');
+  header('Location: oops.html');
   error_log("unable to sign up customer:" . $_POST['stripeEmail'].
     ", error:" . $e->getMessage());
 }
